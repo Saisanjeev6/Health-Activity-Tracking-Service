edd6689f8973a15b87d9dbcaa4ca9e9023e63453
Sai sanjeev Durbakula <sai.sanjeev.durbakula@sap.com>
2024-11-29 16:29:29 +0530

    Made changes to the swagger documentation

--- src/main/java/com/example/trackermicroservice/controller/ActivityExerciseTrackingController.java
+++ src/main/java/com/example/trackermicroservice/controller/ActivityExerciseTrackingController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/health/pet")
 public class ActivityExerciseTrackingController {
